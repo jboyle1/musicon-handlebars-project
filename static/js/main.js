ccc117a30727b006db6fcb6a90a74d85d284c738
Jed Boyle <jedboyle01@gmail.com>
2020-07-07 11:49:00 +0100

    Render the compiled HTML in the browser. Use the document.getElementById() method to get an element with an id of information on the document. Set the innerHTML property on the element returned above to be the compiledHtml.

--- static/js/main.js
+++ static/js/main.js
@@ -50,3 +50,5 @@ const templateSource = templateElement.innerHTML;
 const template = Handlebars.compile(templateSource);
 
 const compiledHtml = template(context);
+
+document.getElementById('information').innerHTML = compiledHtml;
