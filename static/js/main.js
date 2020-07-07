49ebf8a43d9e9578c37fa79afb452af37b7b986f
Jed Boyle <jedboyle01@gmail.com>
2020-07-07 11:45:43 +0100

    After calling Handlebars.compile() with an argument, a function is returned to the template. template will accept an object and use the properties of the object to fill in a Handlebars template. Pass the provided context object into the template function as an argument. Assign the return value of the step above to a new variable named compiledHtml.

--- static/js/main.js
+++ static/js/main.js
@@ -48,3 +48,5 @@ const templateElement = document.getElementById('templateHB');
 const templateSource = templateElement.innerHTML;
 
 const template = Handlebars.compile(templateSource);
+
+const compiledHtml = template(context);
